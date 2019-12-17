68ca9f1dab1c8ea96b8fc36be6406c08dc6e7000
J Larson <shaymus14@gmail.com>
2019-12-17 11:27:30 -0600

    First week of Coursera Capstone Project

---
+++ Coursera_Capstone_wk1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone Project 
+# ## by Jamie Larson
+# 
+# This notebook will be used to complete the capstone project.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# Hello Capstone Project Course!
+
+# In[ ]:
+
+
+
+
